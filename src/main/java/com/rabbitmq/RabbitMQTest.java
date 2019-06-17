9f6a92723850503a67d15c2335794860d3741f75
Jason Young <jasonyeong790@gmail.com>
2019-06-17 15:24:46 +0900

    EmitLogTopic, ReceiveLogsTopic added

--- src/main/java/com/rabbitmq/RabbitMQTest.java
+++ src/main/java/com/rabbitmq/RabbitMQTest.java
@@ -1,7 +1,9 @@
 package com.rabbitmq;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.TimeoutException;
 
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -15,6 +17,7 @@ public class RabbitMQTest {
 	
 	private final static String CONNECTION_HOST = "localhost";	
 	private static final String EXCHANGE_NAME = "logs";
+	private static final String EXCHANGE_NAME2 = "topic_logs";
 	private final static String TASK_QUEUE_NAME = "task_queue";
     private final static String QUEUE_NAME = "hello";
 
@@ -31,6 +34,9 @@ public class RabbitMQTest {
     	
     	//EmitLog(argv);
     	//ReceiveLogs();
+    	
+    	//EmitLogTopic(argv);
+    	//ReceiveLogsTopic(argv);
     }	
     // -----------------------------------------------------------------------------------
     
@@ -45,13 +51,100 @@ public class RabbitMQTest {
     }
     // -----------------------------------------------------------------------------------
     
+
+        
+    // -----------------------------------------------------------------------------------
+    // Messages sent to a topic exchange can't have an arbitrary routing_key - it must be a list of words, delimited by dots. 
+    // The words can be anything, but usually they specify some features connected to the message. 
+    // A few valid routing key examples: "stock.usd.nyse", "nyse.vmw", "quick.orange.rabbit". There can be as many words in the routing key as you like, up to the limit of 255 bytes.
+    public static void EmitLogTopic(String[] argv) throws UnsupportedEncodingException, IOException, TimeoutException {
+    	Channel channel = createNewChannel();
+    	channel.exchangeDeclare(EXCHANGE_NAME2, "topic");
+
+        String routingKey = getRouting(argv);
+        String message = getMessage(argv);
+
+        channel.basicPublish(EXCHANGE_NAME2, routingKey, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
+    }
+    // -----------------------------------------------------------------------------------
+    public static void ReceiveLogsTopic(String [] argv) throws IOException, TimeoutException {
+    	Channel channel = createNewChannel();
+    	channel.exchangeDeclare(EXCHANGE_NAME2, "topic");
+        String queueName = channel.queueDeclare().getQueue();
+
+        if (argv.length < 1) {
+            System.err.println("Usage: ReceiveLogsTopic [binding_key]...");
+            System.exit(1);
+        }
+
+        for (String bindingKey : argv) {
+            channel.queueBind(queueName, EXCHANGE_NAME2, bindingKey);
+        }
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" +
+                delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+    }
+    // -----------------------------------------------------------------------------------
+  
+   
+    
+    
+    
+    // -----------------------------------------------------------------------------------
+    // We want to extend that to allow filtering messages based on their severity. 
+    // We will use a direct exchange instead. The routing algorithm behind a direct exchange is simple - 
+    // a message goes to the queues whose binding key exactly matches the routing key of the message.
+    // We'll use this model for our logging system. Instead of fanout we'll send messages to a direct exchange. 
+    // We will supply the log severity as a routing key. That way the receiving program will be able to select the severity it wants to receive.
+    public static void EmitLogDirect(String[] argv) throws IOException, TimeoutException {
+    	Channel channel = createNewChannel();
+    	channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+
+        String severity = getSeverity(argv);
+        String message = getMessage(argv);
+
+        channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes("UTF-8"));
+        System.out.println(" [x] Sent '" + severity + "':'" + message + "'");
+    }
+    // -----------------------------------------------------------------------------------
+    public static void ReceiveLogsDirect(String [] argv) throws IOException, TimeoutException {
+    	Channel channel = createNewChannel();
+    	String queueName = channel.queueDeclare().getQueue();
+
+        if (argv.length < 1) {
+        	System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
+        	System.exit(1);
+        }
+
+        for (String severity : argv) {
+        	channel.queueBind(queueName, EXCHANGE_NAME, severity);
+        }
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
+    }
+    // -----------------------------------------------------------------------------------
+    
     
     
     
     
     // -----------------------------------------------------------------------------------
+    // The most important change is that we now want to publish messages to our logs exchange instead of the nameless one.
     public static void EmitLog(String[] argv) throws IOException, TimeoutException {
-    	// The most important change is that we now want to publish messages to our logs exchange instead of the nameless one.
+    	
     	Channel channel = createNewChannel();
     	channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
     	String message = argv.length < 1 ? "info: Hello World!" :
@@ -79,10 +172,13 @@ public class RabbitMQTest {
     
     
     
+    
+    
 	// -----------------------------------------------------------------------------------
+    // We will slightly modify the Send function from our previous example, to allow arbitrary messages to be sent from the command line. 
+ 	// This program will schedule tasks to our work queue, so let's name it  newTask
     public static void newTask(String[] argv) throws IOException, TimeoutException {
-    	// We will slightly modify the Send.java code from our previous example, to allow arbitrary messages to be sent from the command line. 
-    	// This program will schedule tasks to our work queue, so let's name it  newTask
+    	
     	Channel channel = createNewChannel();
     	String message = String.join(" ", argv);
     	
@@ -97,10 +193,10 @@ public class RabbitMQTest {
     	System.out.println(" [x] Sent '" + message + "'");
     }
     // -----------------------------------------------------------------------------------
+    // Our old Recv function also requires some changes: it needs to fake a second of work for every dot in the message body. 
+	// It will handle delivered messages and perform the task, so let's call it
     public static void Worker() throws IOException, TimeoutException {
    
-    	// Our old Recv.java program also requires some changes: it needs to fake a second of work for every dot in the message body. 
-    	// It will handle delivered messages and perform the task, so let's call it
     	Channel channel = createNewChannel();
         channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
@@ -130,8 +226,8 @@ public class RabbitMQTest {
     		channel.basicConsume(TASK_QUEUE_NAME, durable, deliverCallback, consumerTag -> { });
     }
     // -----------------------------------------------------------------------------------
+    // Our fake task to simulate execution time:
     private static void doWork(String task) throws InterruptedException {
-    	// Our fake task to simulate execution time:
     	 for (char ch : task.toCharArray()) {
     		 if (ch == '.') {
     			 try {
@@ -143,6 +239,7 @@ public class RabbitMQTest {
    	    }
     }
     // -----------------------------------------------------------------------------------
+    
     
     
     
@@ -172,6 +269,40 @@ public class RabbitMQTest {
     }
     // -----------------------------------------------------------------------------------
     
+    
+    
+    
+    
+    // -----------------------------------------------------------------------------------
+    private static String getRouting(String[] strings) {
+        if (strings.length < 1)
+            return "anonymous.info";
+        return strings[0];
+    }
+    // -----------------------------------------------------------------------------------
+    private static String getSeverity(String[] strings) {
+        if (strings.length < 1)
+            return "info";
+        return strings[0];
+    }
+    // -----------------------------------------------------------------------------------
+    private static String getMessage(String[] strings) {
+        if (strings.length < 2)
+            return "Hello World!";
+        return joinStrings(strings, " ", 1);
+    }
+    // -----------------------------------------------------------------------------------
+    private static String joinStrings(String[] strings, String delimiter, int startIndex) {
+        int length = strings.length;
+        if (length == 0) return "";
+        if (length <= startIndex) return "";
+        StringBuilder words = new StringBuilder(strings[startIndex]);
+        for (int i = startIndex + 1; i < length; i++) {
+            words.append(delimiter).append(strings[i]);
+        }
+        return words.toString();
+    }
+    // -----------------------------------------------------------------------------------
     
     
     
